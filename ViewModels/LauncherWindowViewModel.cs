6f6b2b594e2bd14970ec5e312f5825bce91e424a
joris.gross <gross@sinceideagames.de>
2018-09-10 02:04:39 +0200

    Kleinigkeiten (WIP, funktioniert noch nicht richtig)

--- ViewModels/LauncherWindowViewModel.cs
+++ ViewModels/LauncherWindowViewModel.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using Avalonia.Markup.Xaml.Data;
+using ReactiveUI;
 
 namespace GameLauncherExperimental.ViewModels
 {
     public class LauncherWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        private string _launchButtonName = "Launch";
+        public string LaunchButtonName => _launchButtonName;
+        private int _progressBarValue = 10;
+        public int ProgressBarValue => _progressBarValue;
+        
+        public LauncherWindowViewModel()
+        {
+            LaunchButton = ReactiveCommand.Create(LaunchButtonPressed);
+        }
+        public ReactiveCommand<Unit, Unit> LaunchButton { get; }
+        void LaunchButtonPressed()
+        {
+            _launchButtonName = "Launching..";
+            _progressBarValue += 10;
+            Console.WriteLine(ProgressBarValue);
+
+        }
     }
 }
